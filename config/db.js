a01f4854be5d6974defd9d6753eced407ca7910d
luckyray0314 <saivietthanh0314@gamil.com>
2025-01-21 09:12:54 +0200

    backend basic structure

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const config = require('config');
+const mongoose = require('mongoose');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+
+        console.log("MongoDB connected ...");
+    }
+    catch(err){
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
